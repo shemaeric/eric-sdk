0bba4297e3d0d1a60617dbd5f95c50b2c5c8b891
shemaeric <shemaeric01@gmail.com>
2023-04-18 20:37:23 +0200

    Add Comments to what classes and methods do

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -3,9 +3,15 @@ import { Base } from '../client';
 import { MovieService } from '../services';
 import { applyMixins } from '../utils';
 
+/**
+ * SDK class representing our SDK.
+ * Extends the Base class which handles API requests.
+ * Implements the MovieService interface for movie-related functionality.
+ */
 class SDK extends Base {}
 interface SDK extends MovieService {}
 
+// Apply the MovieService methods to the SDK class using mixins.
 applyMixins(SDK, [MovieService]);
 
 export default SDK;
--- src/utils/mixins.ts
+++ src/utils/mixins.ts
@@ -1,3 +1,11 @@
+/**
+ * Applies mixins to a derived class by copying properties and methods from one or more base classes.
+ * @function
+ * @name applyMixins
+ * @param {any} derivedCtor - The derived class to apply mixins to.
+ * @param {any[]} constructors - An array of base classes to copy properties and methods from.
+ * @returns {void}
+ */
 export function applyMixins(derivedCtor: any, constructors: any[]) {
     constructors.forEach((baseCtor) => {
       Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
